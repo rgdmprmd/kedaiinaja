687fb17bd996c4c3192ce60b7b7461b007bf36c4
Rangga Dimas Permadi <ranggadpermadi@gmail.com>
2021-06-05 17:54:02 +0700

    db update

--- kedaiinaja (1).sql
+++ kedaiinaja (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 05, 2021 at 11:44 AM
+-- Generation Time: Jun 05, 2021 at 12:53 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
